e1ec4bc4254b3bbd7978d849e49c2221fc132389
yunqi <guo.yunq@northeastern.edu>
2023-02-19 13:30:08 -0800

    the search results will not be cleared due to rotation

--- app/src/main/java/edu/northeastern/hikerhub/A6.java
+++ app/src/main/java/edu/northeastern/hikerhub/A6.java
@@ -34,6 +34,8 @@ public class A6 extends AppCompatActivity {
     private Context context;
     private A6Adapter adapter;
     private ProgressBar progressBar;
+    private RecyclerView recyclerView;
+    private CountryList list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,24 +44,53 @@ public class A6 extends AppCompatActivity {
         nameEditText = findViewById(R.id.edit_text_name_input);
         progressBar = findViewById(R.id.progressBar);
         progressBar.setVisibility(View.INVISIBLE);
-        RecyclerView recyclerView = findViewById(R.id.recyclerview);
+        recyclerView = findViewById(R.id.recyclerview);
         adapter = new A6Adapter();
         context = this;
         initRecyclerView(recyclerView, new LinearLayoutManager(this), adapter);
         Retrofit retrofit = new Retrofit.Builder().baseUrl(URL).addConverterFactory(GsonConverterFactory.create()).build();
         service = retrofit.create(A6Service.class);
+        if (savedInstanceState != null) {
+            ArrayList<String> id;
+            double[] pro;
+            id = savedInstanceState.getStringArrayList("id");
+            pro = savedInstanceState.getDoubleArray("pro");
+            List<Country> countries = new ArrayList<>();
+            for (int i = 0; i < id.toArray().length; i++) {
+                countries.add(new Country(id.get(i), pro[i]));
+            }
+            list = new CountryList();
+            list.setCountry(countries);
+            adapter.setCountries(countries);
+            adapter.notifyDataSetChanged();
+            progressBar.setVisibility(View.INVISIBLE);
+        }
+    }
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        ArrayList<String> id = new ArrayList<>();
+        double[] pro = new double[list.getCountry().toArray().length];
+        for (int i = 0; i < list.getCountry().toArray().length; i++) {
+            id.add(list.getCountry().get(i).getCountry_id());
+            pro[i] = list.getCountry().get(i).getProbability();
+        }
+
+        savedInstanceState.putStringArrayList("id", id);
+        savedInstanceState.putDoubleArray("pro", pro);
     }
     public void initRecyclerView(RecyclerView recyclerView, RecyclerView.LayoutManager manager, RecyclerView.Adapter adapter) {
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(manager);
     }
+
     public void search(View view) {
         progressBar.setVisibility(View.VISIBLE);
         String name = nameEditText.getText().toString();
         service.listCountry(name).enqueue(new Callback<CountryList>() {
             @Override
             public void onResponse(Call<CountryList> call, Response<CountryList> response) {
-                CountryList list = response.body();
+                list = response.body();
                 adapter.setCountries(list.getCountry());
                 adapter.notifyDataSetChanged();
 //                for(Country country: list.getCountry()){
