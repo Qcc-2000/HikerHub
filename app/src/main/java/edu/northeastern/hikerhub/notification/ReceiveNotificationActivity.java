1aad268fa44c9a353b00101c48ade792a23d1cf4
zilongzhao <zhao.zilo@northeastern.edu>
2023-03-05 13:34:45 -0800

    added notification, need more work

---
+++ app/src/main/java/edu/northeastern/hikerhub/notification/ReceiveNotificationActivity.java
@@ -0,0 +1,16 @@
+package edu.northeastern.hikerhub.notification;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import edu.northeastern.hikerhub.R;
+
+public class ReceiveNotificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receive_notification);
+    }
+}
--- app/src/main/java/edu/northeastern/hikerhub/stickerService/SendStickerActivity.java
+++ app/src/main/java/edu/northeastern/hikerhub/stickerService/SendStickerActivity.java
@@ -2,8 +2,15 @@ package edu.northeastern.hikerhub.stickerService;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.graphics.PorterDuff;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -30,6 +37,7 @@ import java.util.Map;
 import java.util.UUID;
 
 import edu.northeastern.hikerhub.R;
+import edu.northeastern.hikerhub.notification.ReceiveNotificationActivity;
 import edu.northeastern.hikerhub.stickerService.models.Event;
 import edu.northeastern.hikerhub.stickerService.models.User;
 import edu.northeastern.hikerhub.utils.Utils;
@@ -52,6 +60,11 @@ public class SendStickerActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // new
+        createNotificationChannel();
+        // new end
+
         setContentView(R.layout.activity_send_sticker);
         serverKey = "key=" + Utils.getProperties(getApplicationContext()).getProperty("SERVER_KEY");
         // Connects firebase
@@ -111,6 +124,11 @@ public class SendStickerActivity extends AppCompatActivity {
                 selectedImage.getDrawable().clearColorFilter();
                 Log.d("logInfo=:",receiverUserName);
                 sendSticker(senderUserName, receiverUserName, stickerId);
+
+                // notification
+                createNotificationChannel();
+                sendNotification(view);
+                // notification end
             }
         });
 
@@ -231,4 +249,53 @@ public class SendStickerActivity extends AppCompatActivity {
         }
         // TODO:
     }
+
+    // notification
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is not in the Support Library.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(getString(R.string.channel_id), name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system. You can't change the importance
+            // or other notification behaviors after this.
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+    public void sendNotification(View view) {
+        // Prepare intent which is triggered if the
+        // notification is selected
+        Intent intent = new Intent(this, ReceiveNotificationActivity.class);
+        PendingIntent pIntent = PendingIntent.getActivity(this, (int) System.currentTimeMillis(), intent, 0);
+
+        PendingIntent callIntent = PendingIntent.getActivity(this, (int)System.currentTimeMillis(),
+                new Intent(this, ReceiveHistoryActivity.class), 0);
+
+
+        // Build notification
+        // Actions are just fake
+        String channelId = getString(R.string.channel_id);
+
+//        Notification noti = new Notification.Builder(this)   DEPRECATED
+        Notification noti = new NotificationCompat.Builder(this,channelId)
+
+                .setContentTitle("New mail from " + "test@gmail.com")
+                .setContentText("Subject").setSmallIcon(R.drawable.ic_launcher_foreground)
+
+                .addAction(R.drawable.ic_launcher_foreground, "Call", callIntent).setContentIntent(pIntent).build();
+//                .addAction(R.drawable.icon, "More", pIntent)
+//              .addAction(R.drawable.icon, "And more", pIntent).build();
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        // hide the notification after its selected
+        noti.flags |= Notification.FLAG_AUTO_CANCEL ;
+
+        notificationManager.notify(0, noti);
+    }
+    //notification end
 }
